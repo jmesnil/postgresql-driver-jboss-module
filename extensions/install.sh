ed0b3e629ddb4e2ccd56f759f99d713209331960
Jeff Mesnil <jmesnil@redhat.com>
2021-02-11 09:19:28 +0100

    rename driver to postgresql
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- extensions/install.sh
+++ extensions/install.sh
@@ -5,6 +5,7 @@ if [ "${SCRIPT_DEBUG}" = "true" ] ; then
     echo "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
 fi
 
+echo "Installing PostgreSQL JDBC driver from the postresql-driver-jboss-module image"
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
 install_modules ${injected_dir}/modules
